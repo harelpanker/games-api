e16314fbe102816271585fb109414211b5316455
harelpanker <harelpanker@gmail.com>
2023-06-02 17:58:00 +0200

    progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,13 @@
 //https://rawg.io/@harel/apikey
 
+import { getGames } from '@/lib/get_games';
 import TypographyH1 from '../components/typography/TypographyH1';
 import GameCard from './GameCard';
 
-type GameData = {
-  count: number;
-  next: string;
-  previous: string;
-  results: {
-    id: number;
-    name: string;
-    background_image: string;
-    rating: number;
-  }[];
-};
-
-const getGames = async (): Promise<GameData> => {
-  const response = await fetch(
+export default async function Home() {
+  const games = await getGames(
     `https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}&page_size=12&page=2`
   );
-  if (!response.ok) throw new Error(response.statusText);
-  const data = await response.json();
-
-  return data;
-};
-
-export default async function Home() {
-  const games = await getGames();
 
   return (
     <main className='py-20 px-5'>
---
+++ lib/get_games.ts
@@ -0,0 +1,19 @@
+type GameData = {
+  count: number;
+  next: string;
+  previous: string;
+  results: {
+    id: number;
+    name: string;
+    background_image: string;
+    rating: number;
+  }[];
+};
+
+export const getGames = async (url: string): Promise<GameData> => {
+  const response = await fetch(url, { cache: 'no-store' });
+  if (!response.ok) throw new Error(response.statusText);
+  const data = await response.json();
+
+  return data;
+};
